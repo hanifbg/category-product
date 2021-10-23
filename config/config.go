9b47f68f4a1c33bbc43a592b112e7d37f92d0e1f
hanifbg <hanifbg48@gmail.com>
2021-10-23 11:09:07 +0700

    change default config

--- config/config.go
+++ config/config.go
@@ -55,7 +55,7 @@ func initConfig() *AppConfig {
 	defaultConfig.DbPort = 3306
 	defaultConfig.DbUsername = "root"
 	defaultConfig.DbPassword = "1"
-	defaultConfig.DbName = "alta_final"
+	defaultConfig.DbName = "db_name"
 
 	viper.AutomaticEnv()
 	viper.SetEnvPrefix("your")
